fef117bd8bc824676ca7c59c337d2331437a7b15
ali gudam <60675762+sam34512@users.noreply.github.com>
2020-02-25 19:04:54 -0500

    second commit

--- sam34512.github.io/src/App.js
+++ sam34512.github.io/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          my name is ali guddam and i am a master in react 
         </p>
         <a
           className="App-link"
